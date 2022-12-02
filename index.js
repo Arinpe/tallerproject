04203f472cec669099974773dcc8ecfdaada8333
Arinpe <dehtolah@gmail.com>
2022-12-02 23:58:15 +0100

    push

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const links = document.querySelectorAll(".nav-link");
 const contents = document.querySelectorAll(".content");
 const hideContents = () =>
   contents.forEach((content) => {
-    content.classList.remove("show");
+    console.log(content);
+    content.classList.remove("show-content");
   });
 links.forEach((link) => {
   link.addEventListener("click", (e) => {
     hideContents();
-    document.querySelector(`#${e.target.dataset.target}`).classList.add("show");
+    document.querySelector(`#${link.dataset.target}`).classList.add("show-content");
   });
 });
